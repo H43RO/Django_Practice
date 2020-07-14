8cfb14a45b6636ea01652b58cc7cd2da27a36330
H43RO <h2is1234@naver.com>
2020-07-14 22:24:39 +0900

    간단한 URL Mapping, Templating 연습

--- dJango_Practice/urls.py
+++ dJango_Practice/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 from first import views
 
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include('first.urls')),
     path('admin/', admin.site.urls),
 ]
--- first/views.py
+++ first/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.template import loader
+from datetime import datetime
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    template = loader.get_template('index.html')
+    now = datetime.now()
+
+    # 응답으로 전달하고 싶은 데이터
+    context = {
+        'current_date' : now
+    }
+
+    return HttpResponse(template.render(context, request))
+
+
+def select(request):
+    message = '수 하나를 입력해주세요'
+    return HttpResponse(message)
+
+
+def result(request):
+    message = "추첨 결과입니다."
+    return HttpResponse(message)
